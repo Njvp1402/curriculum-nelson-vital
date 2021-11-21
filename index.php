a14000f9c5048443e532dcd5be35b99872ac9df6
Njvp1402 <73442673+Njvp1402@users.noreply.github.com>
2021-11-21 19:15:46 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nelson Vital</title>
+    <style>
+        body{
+            background-image:url("C:/Users/usuario/Desktop/curriculum/fondo 2.jpg")  ;
+            background-size: cover;
+            border: 1px solid rgb(1, 1, 7);
+            padding:12px;
+            width: 60%;
+            margin: 0 auto;
+        }
+        fieldset{
+            margin-bottom: 12px; 
+        }
+        legend{
+            background-color: white;
+            border:2px solid blue;
+            padding: 2px;
+            border-radius: 20px;
+            
+        }
+        img{
+            border-radius: 5%;
+            float: left;
+            margin-right: 50px;
+            margin-bottom: 5%;
+            
+
+              
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <img src="imagen.jpg" width="100" height="150" >
+        <h1>Nelson Jose Vital Perez</h1>
+        <p>Las Mercedes, calle 5, casa 2, Villa de Cura, Edo. Aragua</p>
+        <p>Telf: (0414)4430301</p>
+        <fieldset>
+            <legend><h2>Datos Generales</h2></legend>
+            <ul>
+                <li>fecha de nacimiento: 12/03/1998</li>
+                <li>Cedula:V-26039298</li>
+                <li>E-mail:nelsonvital12@gmail.com</li>
+            </ul>
+        </fieldset>
+        <fieldset>
+            <legend><h2>Formacion Academica:</h2></legend>
+            <ul>
+                <li>Bachillerato: U.E.P San Luis Rey</li>
+                <li>Universidad: Universidad Nacional Experimental Romulo Gallegos </li>
+                <li>cursos realizados: Academia de Sistemas Maracay</li>
+            </ul>
+        </fieldset>
+        <fieldset>
+            <legend><h2>Experiencia laboral</h2></legend>
+            <li>vendedor:Sumunistros Orion</li>
+
+        </fieldset>
+    </div>
+    
+</body>
+</html>
